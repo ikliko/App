cc2ae55333c537880fdf8abcc2228f17274ba933
Kiril Atanasov <kliko.atanasov@gmail.com>
2017-04-02 00:56:40 +0300

    Application api uploading - Oauth 2 - Cors easy setup

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+        factory(\App\User::class, 10)->create();
+        \App\User::create([
+            'name' => 'Kliko Atanasov',
+            'email'=> 'kliko.atanasov@gmail.com',
+            'password' => \Illuminate\Support\Facades\Hash::make('123456')
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/test', function () {
+    dd(phpinfo());
+    return response()->json([
+        'user' => [
+            'firstName'=> 'Kliko',
+            'lastName' => 'Atanasov'
+        ]
+    ]);
+});
